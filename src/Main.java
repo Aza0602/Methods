a6fda5f1fa58f5fced356fa0bb99f7762f844d1c
Azat <aelchikushuulu@gmail.com>
2023-03-02 14:20:40 +0600

    Task2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         printInfoLeapYear(2023);
+        printInfoOsMadeYear(0, 2024 );
     }
 
     public static void printInfoLeapYear(int year) {
+
+        System.out.println("Задача 1");
+
         if (isLeapYear(year)) {
             System.out.println(year + " - год високосный");
         } else {
@@ -13,5 +20,18 @@ public class Main {
 
     public static boolean isLeapYear(int year) {
         return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+
+    public static void printInfoOsMadeYear(int clientOS, int deviceYear) {
+
+        System.out.println("Задача 2");
+
+        if (clientOS != 0 & clientOS != 1) {
+            System.out.println("ОС должна иметь значение 0 или 1");
+            return;
+        }
+        int currentYear = LocalDate.now().getYear();
+        String result = "Нужно установить " + (deviceYear < currentYear ? " облегченную версию для " : " версию для ") + (clientOS == 0 ? "iOS" : "Android");
+        System.out.println(result);
     }
 }
